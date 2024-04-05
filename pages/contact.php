9ce7a58c087bc6b3081f19b7cde91ed0ea4df779
ihbzk <pro.akyrox@gmail.com>
2024-04-05 13:33:51 +0200

    Add some modifications

--- pages/contact.php
+++ pages/contact.php
@@ -7,7 +7,7 @@
             <div class="form-outer">
                 <form method="post" id="footer-contact-form">
                     <div class="page slide-page">
-                        <p class="text-contact">Un projet ? Une question? Vous pouvez nous contacter avec le formulaire ci-dessous ou bien par mail à l’adresse <br><a href="mailto:<?= $conf_email ?>" class="footer-link"><?= $conf_email ?></a>.</p><br>
+                        <p class="text-contact">Vous avez un projet en tête ou besoin de renseignements supplémentaires ? N'hésitez pas à nous joindre directement via le formulaire de contact ci-dessous, ou par e-mail à <br><a href="mailto:<?= $conf_email ?>" class="footer-link"><?= $conf_email ?></a>.</p><br>
                         <div class="field">
 				            <?= form_error($data["email"]); ?>
                             <input type="email" class="simple-form" name="email" placeholder="Entrez votre e-mail" value="<?= $data["email"]["value"]; ?>" required />
@@ -16,24 +16,17 @@
 				            <?= form_error($data["phone"]); ?>
                             <input type="tel" class="simple-form" name="phone" placeholder="Entrez votre téléphone" value="<?= $data["phone"]["value"]; ?>" required >
                         </div>
-                        <div class="field btns">
-                            <button class="next-1 next"><img src="<?= asset("img/icons/arrow-diago.svg"); ?>" class="w-100" alt="Logo Play" loading="lazy"></button>
+                        <div class="field">
+                            <?= form_error($data["lastname"]); ?>
+                            <input type="text" class="simple-form" name="lastname" placeholder="Entrez votre nom" value="<?= $data["lastname"]["value"]; ?>">
                         </div>
-                    </div>
-                    <div class="page">
-                        <div class="d-f">
-                            <div class="field">
-                                <?= form_error($data["lastname"]); ?>
-                                <input type="text" class="simple-form" name="lastname" placeholder="Nom" value="<?= $data["lastname"]["value"]; ?>">
-                            </div>
-                            <div class="field">
-                                <?= form_error($data["firstname"]); ?>
-                                <input type="text" class="simple-form" name="firstname" placeholder="Prénom" value="<?= $data["firstname"]["value"]; ?>">
-                            </div>
+                        <div class="field">
+                            <?= form_error($data["firstname"]); ?>
+                            <input type="text" class="simple-form" name="firstname" placeholder="Entrez votre prénom" value="<?= $data["firstname"]["value"]; ?>">
                         </div>
                         <div class="field">
 		                    <?= form_error($data["message"], "Veuillez fournir un message."); ?>
-                            <textarea class="simple-form" name="message" placeholder="Votre message" rows="8" required><?= $data["message"]["value"]; ?></textarea>
+                            <textarea class="simple-form" name="message" placeholder="Entrez votre message" rows="8" required><?= $data["message"]["value"]; ?></textarea>
                         </div>
                         <?= form_error($data["rgpd"], "Veuillez accepter les conditions"); ?>
                         <label for="rgpd">
